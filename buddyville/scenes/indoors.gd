b1f8a53e4c49102694412736b120ebb791e46a7c
starrnakamitsu <nakamits26@up.edu>
2024-11-02 15:05:50 -0700

    fix player exiting building

--- buddyville/scenes/indoors.gd
+++ buddyville/scenes/indoors.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
 func _on_exit_body_entered(body: Node2D) -> void:
-	print("exit")
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	if body == $player:
+		print("exit")
+		get_tree().change_scene_to_file("res://scenes/main.tscn")

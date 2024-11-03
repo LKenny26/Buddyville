429a410709613e7ebca096eb638378fef934a61b
LKenny26 <kenny26@up.edu>
2024-11-02 20:42:58 -0700

    its something

--- buddyville/scripts/mini_map.gd
+++ buddyville/scripts/mini_map.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 	miniMapPlayer.get_node("QuestHUD").queue_free()
 	miniMapPlayer.get_node("Pause_Menu").queue_free()
 	
-	sub_viewport.add_child(tileMap.duplicate())
+	#sub_viewport.add_child(tileMap.duplicate())
 	sub_viewport.add_child(miniMapPlayer)
 	
 func _process(delta: float) -> void:
